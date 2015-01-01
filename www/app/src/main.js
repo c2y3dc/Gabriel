e3f3e56d7c589fbbeef464e1ebd3bf537be9575a
Kingsten Banh <kingsten.banh@gmail.com>
2014-12-31 16:14:38 -0800

    remove unneccessary code

--- www/app/src/main.js
+++ www/app/src/main.js
@@ -3,17 +3,10 @@ define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
-
-    // creat main App View
     var AppView = require('views/AppView');
+
     // create the main context
     var mainContext = Engine.createContext();
-    // set perspective
-    mainContext.setPerspective(1000);
-
     var appView = new AppView();
 
     mainContext.add(appView);
