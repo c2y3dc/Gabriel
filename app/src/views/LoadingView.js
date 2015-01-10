41ab180be3b1ac1a6cfffc27b928badd61ce4e31
Alex Hawkins <alexhawkins.me@gmail.com>
2015-01-09 17:24:20 -0800

    change loading view

--- app/src/views/LoadingView.js
+++ app/src/views/LoadingView.js
@@ -1,35 +1,35 @@
 define(function(require, exports, module) {
-    var View = require('famous/core/View');
-    var Surface = require('famous/core/Surface');
-    var Transform = require('famous/core/Transform');
-    var StateModifier = require('famous/modifiers/StateModifier');
+  var View = require('famous/core/View');
+  var Surface = require('famous/core/Surface');
+  var Transform = require('famous/core/Transform');
+  var StateModifier = require('famous/modifiers/StateModifier');
 
-    function LoadingView() {
-        View.apply(this, arguments);
-        _createLoadingPage.call(this);
-    }
+  function LoadingView() {
+    View.apply(this, arguments);
+    _createLoadingPage.call(this);
+  }
 
-    LoadingView.prototype = Object.create(View.prototype);
-    LoadingView.prototype.constructor = LoadingView;
+  LoadingView.prototype = Object.create(View.prototype);
+  LoadingView.prototype.constructor = LoadingView;
 
-    LoadingView.DEFAULT_OPTIONS = {};
+  LoadingView.DEFAULT_OPTIONS = {};
 
-    function _createLoadingPage() {
+  function _createLoadingPage() {
 
         this.bodySurface = new Surface({
             size: [undefined, undefined],
             properties: {
-                backgroundColor: 'black'
+                backgroundColor: '#FDFDFD'
             }
         });
-
         this.bodyModifier = new StateModifier({
-            transform: Transform.translate(0, 0, 201)
+            transform: Transform.translate(0, 0, 0.9)
         });
         // this.node.add(this.bodyModifier).add(this.bodySurface);
         this.add(this.bodyModifier).add(this.bodySurface);
 
     }
+  }
 
-    module.exports = LoadingView;
+  module.exports = LoadingView;
 });
