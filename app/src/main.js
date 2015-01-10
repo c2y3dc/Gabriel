cb7eb404b8bf142452d7e500aa211b803ea85640
Ervin Chow <ervinchow@gmail.com>
2015-01-09 17:15:09 -0800

    switch between mobile and browser

--- app/src/main.js
+++ app/src/main.js
@@ -24,11 +24,11 @@ define(function(require, exports, module) {
     else{
 
         //UNCOMMENT THE LINE BELOW WHEN RUNNING ON MOBILE DEVICES
-         //document.addEventListener('DOMContentLoaded', start)
+        document.addEventListener('DOMContentLoaded', start)
 
         // COMMENT OUT BOTH LINES BELOW WHEN RUNNING ON MOBILE DEVICES
-         require('../lib/oauth-js/dist/oauth.min.js');
-         start();
+         // require('../lib/oauth-js/dist/oauth.min.js');
+         // start();
     }
 
 });
